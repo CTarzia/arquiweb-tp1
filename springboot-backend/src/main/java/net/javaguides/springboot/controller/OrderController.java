6e7caa640e448af6a21a2f68877c767b45af42fa
pablo <pcominguez@gmail.com>
2021-10-08 15:26:52 -0300

    problemas con la base de datos

---
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/OrderController.java
@@ -0,0 +1,60 @@
+package net.javaguides.springboot.controller;
+
+        import java.util.HashMap;
+        import java.util.Map;
+
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.http.ResponseEntity;
+        import org.springframework.web.bind.annotation.*;
+
+        import net.javaguides.springboot.exception.ResourceNotFoundException;
+        import net.javaguides.springboot.model.Order;
+        import net.javaguides.springboot.repository.OrderRepository;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/order/")
+public class OrderController {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    // get one order by id
+    @GetMapping("/{orderid}")
+    public ResponseEntity<Order> getOrder(@PathVariable Long orderid){
+        Order order = orderRepository.findById(orderid)
+                .orElseThrow(() -> new ResourceNotFoundException("Order not exist with id :" + orderid));
+        return ResponseEntity.ok(order);
+    }
+
+    // create order
+    @PostMapping("/{restoid}")
+    public Order createOrder(@RequestBody Order order, @PathVariable Long restoid) {
+        return orderRepository.save(order);
+    }
+
+    // update order rest api
+
+    @PutMapping("/{orderid}")
+    public ResponseEntity<Order> updateOrder(@PathVariable Long orderid, @RequestParam String accept){
+        Order order = orderRepository.findById(orderid)
+                .orElseThrow(() -> new ResourceNotFoundException("Order not exist with id :" + orderid));
+
+        order.setStatus(accept);
+
+        Order updatedOrder = orderRepository.save(order);
+        return ResponseEntity.ok(updatedOrder);
+    }
+
+    // delete order rest api
+    @DeleteMapping("/{orderid}")
+    public ResponseEntity<Map<String, Boolean>> deleteOrder(@PathVariable Long orderid){
+        Order order = orderRepository.findById(orderid)
+                .orElseThrow(() -> new ResourceNotFoundException("Order not exist with id :" + orderid));
+
+        orderRepository.delete(order);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ springboot-backend/src/main/java/net/javaguides/springboot/model/Order.java
@@ -0,0 +1,62 @@
+package net.javaguides.springboot.model;
+
+import javax.persistence.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+//@Inheritance
+//@DiscriminatorColumn(name = "Order_Type")
+@Table(name = "order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "status")
+    private String status;
+
+    public Order() {
+
+    }
+
+    public Order(String content, String status) {
+        super();
+        this.content = content;
+        this.status = status;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
+
