3d8f1acea6fa58ac5c3dfc20f7e8c2faadf5241f
Chiara <dulcechiara@gmail.com>
2021-10-21 19:24:43 -0300

    home

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { StylesProvider } from "@material-ui/styles";
 
 import "./index.scss";
--- react-frontend/src/screens/Home/index.js
+++ react-frontend/src/screens/Home/index.js
@@ -1,14 +1,19 @@
 import React from "react";
-import { Button } from "@mui/material";
+// import { Button } from "@mui/material";
+import {Link } from "react-router-dom";
 
 import styles from "./styles.module.scss";
 
 const Home = () => {
-	const handleOnClick = () => console.log("Hola!");
+	// const handleNearbyRestaurants = () => alert("1");
+	// const handleOrderStatus = () => alert("2");
 	return (
 		<div className={styles.container}>
-			<span className={styles.label}> Bienvenido </span>
-			<Button onClick={handleOnClick}>Hace Click aca</Button>
+			<span className={styles.label}> Bienvenido! </span>
+			
+			<Link to="/nearbyRestaurants"><button>Ver restaurantes cercanos</button></Link>
+
+			<Link to="/orderStatus"><button>Ver estado de pedido</button> </Link>
 		</div>
 	);
 };
