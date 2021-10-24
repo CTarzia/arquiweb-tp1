ec5172717d73df49fb8f11dd6b557ed5a7f03e70
Guido Lipina <guido.lipina@widergy.com>
2021-10-24 15:11:07 -0300

    arreglo de rutas

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import Home from "./screens/Home";
 import OrderForPickup from "./screens/orderforpickup";
 import OrderStatus from "./screens/OrderStatus";
+import { ROUTES } from "./constants/routes";
 
 function App() {
 	return (
@@ -10,8 +12,16 @@ function App() {
 			<Router>
 				<Switch>
 					<Route exact path="/" component={Home}></Route>
-					<Route exact path="/orderforpickup" component={OrderForPickup}></Route>
-					<Route exact path="/orderStatus" component={OrderStatus}></Route>
+					<Route
+						exact
+						path={ROUTES.CREATE_ORDER}
+						component={OrderForPickup}
+					></Route>
+					<Route
+						exact
+						path={ROUTES.ORDER_STATUS}
+						component={OrderStatus}
+					></Route>
 				</Switch>
 			</Router>
 		</Fragment>
---
+++ react-frontend/src/constants/routes.js
@@ -0,0 +1,4 @@
+export const ROUTES = {
+	ORDER_STATUS: "estado_del_pedido",
+	CREATE_ORDER: "/restaurante/:id/hacer_pedido",
+};
